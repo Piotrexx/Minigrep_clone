d07ce928bd2383ef530f5b00249fb52e415762cb
Piort Łopatecki <piotr.lopatecki@gmail.com>
2024-06-13 15:02:07 +0200

    better searching

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 use colored::Colorize;
 
 pub struct Config {
     pub query: String,
-    pub filepath: String
+    pub filepath: String,
+    pub ignore_case: bool,
 }
 
 #[derive(PartialEq)]
@@ -23,20 +24,24 @@ impl Config {
 
         let query = args[1].clone();
         let filepath = args[2].clone();
-    
-        Ok(Config {query, filepath})
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Ok(Config {query, filepath, ignore_case})
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filepath)?;
 
-    let results = search(&config.query, &contents);
-
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+    
     if !results.is_empty(){
         for line in results {
             for word in line.content.split_whitespace(){
-                if word.contains(&config.query){
+                if word.to_lowercase().contains(&config.query.to_lowercase()){
                     print!("{} ", word.red());
                     continue;
                 }
@@ -67,6 +72,23 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<Line> {
     result
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<Line> {
+
+    let mut result = Vec::new();
+    let query = query.to_lowercase();
+
+    for (index,line) in contents.lines().enumerate() {
+        if line.to_lowercase().contains(&query){
+            let found_line = Line {
+                content: line.to_string(), 
+                line: index + 1
+            };
+            result.push(found_line)
+        }
+    }
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -78,6 +100,7 @@ mod tests {
 safe, fast, productive.
 Pick three.";
 
+        // assert_eq!(Line{content: String::from("safe, fast, productive."), line: 5} ,search(query, contents));
         assert_eq!(vec![Line{content: String::from("safe, fast, productive."), line: 2}], search(query, contents))
     }
 }
